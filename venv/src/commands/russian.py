26e79771db1344ed7173c0dc0d84f486631bca75
Christian <chrispalla129@gmail.com>
2020-02-25 18:06:45 -0500

    beginning of RR

---
+++ venv/src/commands/russian.py
@@ -0,0 +1,34 @@
+import os
+import random
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord.ext.commands import has_permissions, CheckFailure
+
+global leader
+global bulletPos
+global curPos
+global players
+global state
+
+async def init(ctx, member: discord.Member):
+    global leader
+    global bulletPos
+    global curPos
+
+    leader = member
+    bulletPos = random.randint(1, 6)
+    curPos = random.randint(1, 6)
+    state = "join"
+    await ctx.send("Game Initialized! Join with '--roulette join' to play.")
+
+
+async def join(ctx, member: discord.Member):
+    global players
+    players.append(member)
+
+async def start(ctx):
+
+
+
+
