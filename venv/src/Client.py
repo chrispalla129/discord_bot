4534f04a8379106da33fd8b3b3e15867aa8e9594
Julius <jbreindl@buffalo.edu>
2020-02-25 16:08:02 -0500

    connor is the math man

--- venv/src/Client.py
+++ venv/src/Client.py
@@ -11,15 +11,22 @@ client = discord.Client()
 
 
 @client.event
+# bot init
 async def on_ready():
     print(f'{client.user} has connected to Discord!')
-    for guild in client.guilds:
-        if guild.name==GUILD:
-            break
+    guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
 
     print(
         f'{client.user} is connected to the following guild:\n'
         f'{guild.name}(id: {guild.id})'
     )
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == 'Connor':
+        response = "The math man himself"
+        await message.channel.send(response)
 
 client.run(token)
